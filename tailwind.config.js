987568a353f371384f4d24c449b6017b015f6a8a
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-30 17:51:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        andada: ["Andada Pro", "serif"],
+        anonymous : ["Anonymous Pro", "monospace"],
+        cormorant: ["Cormorant", "serif"],
+        cutive : ["Cutive Mono", "monospace"],
+        encode: ["Encode Sans", "sans-serif"],
+        firacode : ["Fira Code", "monospace"],
+        hahmlet: ["Hahmlet", "serif"],
+        inconsolata: ["Inconsolata", "monospace"],
+        lato: ["Lato", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        notosans:["Noto Sans Mono", "monospace"],
+        playfair: ["Playfair Display", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        robotoslab: ["Roboto Slab", "serif"],
+        sourcecode: ["Source Code Pro", "monospace"],
+        titillium: ["Titillium Web", "sans-serif"],
+        nanum: ["Nanum Gothic Coding", "monospace"],
+        ubuntu: ["Ubuntu Mono", "monospace"],
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0d6efd",
+          paste: "#2abba7",
+          secondary: "#6c757d",
+          success: "#198754",
+          danger: "#dc3545",
+          warning: "#ffc107",
+          info: "#0dcaf0",
+          light: "#f8f9fa",
+          dark: "#212529",
+          darkLight: "#343a40",
+          text: "#212529",
+          background: "#ffffff",
+          border: "#dee2e6",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          error: "#dc3545",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
